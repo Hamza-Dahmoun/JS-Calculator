78d89c5f4d82a825877fcc3956cf0c2bc630c0c9
Hamza <hamza.dahmoun7@gmail.com>
2019-10-28 23:46:16 +0100

    Considering float numbers as userInput

--- JS Calculator.js
+++ JS Calculator.js
@@ -105,7 +105,7 @@ function convertInputToArray(input) {
 
 
 function isNumber(char) {
-    if (char != "0" && char != "1" && char != "2" && char != "3" && char != "4" && char != "5" && char != "6" && char != "7" && char != "8" && char != "9")
+    if (char != "." && char != "0" && char != "1" && char != "2" && char != "3" && char != "4" && char != "5" && char != "6" && char != "7" && char != "8" && char != "9")
         return false
     else return true;
 }
